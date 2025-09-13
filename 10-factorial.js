cad3120e4cff134c571c8001d8da2a72f311e02a
Ezekiel john <johnezekiel526@gmail.com>
2025-09-13 12:58:14 -0700

    zzzz

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2], 10);
+console.log(factorial(arg));
